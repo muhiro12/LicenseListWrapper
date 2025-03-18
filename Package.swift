d760514bee0ba669db4aed8362d64a64ff17810d
muhiro_ <muhiro.12@gmail.com>
2025-03-18 10:32:22 +0900

    Update packages

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/cybozu/LicenseList.git", branch: "main")
+        .package(url: "https://github.com/cybozu/LicenseList.git", "2.0.0"..<"3.0.0")
     ],
     targets: [
         .target(
